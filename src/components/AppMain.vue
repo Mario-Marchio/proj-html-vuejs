f433d793c3a8b89bd8a8496a17fc0f38044261fa
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-12 02:37:10 +0200

    section 2 and cards done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,40 +9,105 @@
     </div>
     <div class="container-medium">
         <div class="d-flex justify-content-around">
-            <div class="text-center">
+            <div class="text-center ">
                 <img src="../assets/img/icon-1.png" alt="Same Day Appointments">
                 <div class="change-color">SAME DAY APPONTMENTS</div>
-                <div>
+                <div class="pb-5">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aperiam quia id animi
                 </div>
             </div>
-            <div class="text-center">
+            <div class="text-center ">
                 <img src="../assets/img/icon-2.png" alt="Same Day Appointments">
                 <div class="change-color">WORLD CLASS FACILITIES</div>
-                <div>
+                <div class="pb-5">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aperiam quia id animi
                 </div>
             </div>
-            <div class="text-center">
+            <div class="text-center ">
                 <img src="../assets/img/icon-3.png" alt="Same Day Appointments">
                 <div class="change-color">EXPERT DOCTORS</div>
-                <div>
+                <div class="pb-5">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aperiam quia id animi
                 </div>
             </div>
-            <div class="text-center">
+            <div class="text-center ">
                 <img src="../assets/img/icon-4.png" alt="Same Day Appointments">
                 <div class="change-color">COMPLEMENTARY THERAPIES</div>
-                <div>
+                <div class="pb-5">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aperiam quia id animi
                 </div>
             </div>
 
         </div>
     </div>
+    <div class="section-2">
+        <div class="container-small text-center">
+            <img class="pt-5 pb-3" src="../assets/img/icon-5.png" alt="doctor face">
+            <div class="fs-3 mb-3">MEET OUR DOCTOR</div>
+            <div class="grey-line"></div>
+            <div class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cumque perferendis maiores
+                ducimus!
+                Officiis doloremque laudantium nemo autem sit facere, ea ex aut illo</div>
+        </div>
+        <div class="container-medium d-flex mt-5">
+            <div class="card mx-3" style="width: 18rem;">
+                <img src="../assets/img/doctor-1.jpg" alt="jon snow">
+                <div class="card-body">
+                    <h5 class="card-title">JON SNOW</h5>
+                    <h5 class="card-title">anestethiologist</h5>
+                    <p class="card-text py-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, </p>
+                    <p class="card-text mt-4 d-flex">
+                        <i class="fa-brands fa-square-facebook fa-xl mx-1" style="color: #327ec7;"></i>
+                        <i class="fa-brands fa-square-twitter fa-xl mx-1" style="color: #3896c2;"></i>
+                        <i class="fa-brands fa-square-instagram fa-xl mx-1" style="color: #245ab7;"></i>
+                    </p>
+                </div>
+            </div>
+            <div class="card mx-3" style="width: 18rem;">
+                <img src="../assets/img/doctor-2.jpg" alt="jon snow">
+                <div class="card-body">
+                    <h5 class="card-title">TONY STARK</h5>
+                    <h5 class="card-title">cardiologist</h5>
+                    <p class="card-text py-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, </p>
+                    <p class="card-text mt-4 d-flex">
+                        <i class="fa-brands fa-square-facebook fa-xl mx-1" style="color: #327ec7;"></i>
+                        <i class="fa-brands fa-square-twitter fa-xl mx-1" style="color: #3896c2;"></i>
+                        <i class="fa-brands fa-square-instagram fa-xl mx-1" style="color: #245ab7;"></i>
+                    </p>
+                </div>
+            </div>
+            <div class="card mx-3" style="width: 18rem;">
+                <img src="../assets/img/doctor-3.jpg" alt="jon snow">
+                <div class="card-body">
+                    <h5 class="card-title">ANNA SMITH</h5>
+                    <h5 class="card-title">nurse practioner</h5>
+                    <p class="card-text py-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, </p>
+                    <p class="card-text mt-4 d-flex">
+                        <i class="fa-brands fa-square-facebook fa-xl mx-1" style="color: #327ec7;"></i>
+                        <i class="fa-brands fa-square-twitter fa-xl mx-1" style="color: #3896c2;"></i>
+                        <i class="fa-brands fa-square-instagram fa-xl mx-1" style="color: #245ab7;"></i>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </template>
-<style>
+<style lang="scss" scoped>
 .change-color:hover {
     color: #3bafbf;
+}
+
+.section-2 {
+    background-color: #e5e5e5;
+    height: 1400px;
+}
+
+.grey-line {
+
+    background-color: #dddddd;
+    width: 200px;
+    height: 4px;
+    margin-left: 150px
 }
 </style>
